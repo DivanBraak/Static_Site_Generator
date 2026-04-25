c5a9fa695e4f7a78836c45dda20abd6bde61c32a
DivanB <divan.braak@gmail.com>
2026-04-25 22:34:07 +0200

    Added leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,3 @@
-
-
 class HTMLNode():
     def __init__(self,tag=None,value=None,children=None,props=None):
         self.tag = tag
@@ -26,6 +24,28 @@ class HTMLNode():
         print_lines.append(f"TAG: {self.tag}")
         print_lines.append(f"VALUE: {self.value}")
         print_lines.append(f"CHILDREN: {self.children}")
+        print_lines.append(f"PROPS: {self.props}")
+        print_lines.append("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        return "\n".join(print_lines)
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+    
+    def to_html(self):
+        if not self.value: 
+            raise ValueError("ERROR: Leafnode has no value")
+        
+        if not self.tag:
+            return self.value
+        
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        print_lines = []
+        print_lines.append("xxxxxxxxxxxxxxxxxxHTML NODExxxxxxxxxxxxxxxxxxxxxxxx")
+        print_lines.append(f"TAG: {self.tag}")
+        print_lines.append(f"VALUE: {self.value}")
         print_lines.append(f"PROPS: {self.props}")
         print_lines.append("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
         return "\n".join(print_lines)
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html(self):
@@ -22,3 +22,14 @@ class TestHTMLNode(unittest.TestCase):
         "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
         ])
         self.assertEqual(repr(node), expected)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_no_value(self):
+        node = LeafNode("p", None)
+        self.assertRaises(ValueError, node.to_html)
+
+if __name__ == "__main__":
+    unittest.main()
