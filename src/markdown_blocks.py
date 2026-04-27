33204dfc0cd519938ce03627e5d39a1bf0caf0d5
DivanB <divan.braak@gmail.com>
2026-04-27 11:00:29 +0200

    added blocktype checking and tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+
 def markdown_to_blocks(markdown):
     raw_blocks = markdown.split('\n\n')
     blocks = []
@@ -7,3 +19,45 @@ def markdown_to_blocks(markdown):
             blocks.append(block.strip())  
 
     return blocks
+
+def block_to_block_type(block):
+    
+    #HEADING CHECK (DONE)
+    count = (len(block) - len(block.lstrip('#')))
+    if 0 < count < 7:
+        if count < len(block) and block[count] == " ":
+            return BlockType.HEADING
+    
+    #CODE CHECK (DONE)
+    if block.startswith('```\n') and block.endswith('```'):
+        return BlockType.CODE
+    
+    #QUOTE CHECK (DONE)
+    lines = block.split('\n')
+    quote = True
+    for line in lines:
+        if not line.startswith('>'):
+            quote = False
+    if quote:
+        return BlockType.QUOTE
+    
+    #UNORDERED_LIST CHECK 
+    lines = block.split('\n')
+    ul = True
+    for line in lines:
+        if not line.startswith('- '):
+            ul = False
+    if ul:
+        return BlockType.UNORDERED_LIST
+    
+    #ORDERED_LIST CHECK 
+    lines = block.split('\n')
+    ol = True
+    for i in range(len(lines)):
+        expected = str((i+1))+'. '
+        if not lines[i].startswith(expected):
+            ol = False
+    if ol:
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks,block_to_block_type,BlockType
 
 class test_blocks(unittest.TestCase):
     def test_normal_usage(self):
@@ -85,7 +85,183 @@ This is the same paragraph on a new line
             ["This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line"]
         )
 
+class test_block_to_block_type(unittest.TestCase):
+    #HEADING TESTS
+    def test_heading1(self):
+        input = "# Heading 1"
 
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_heading6(self):
+        input = "###### Heading 6"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_heading_too_many_hash(self):
+        input = "####### Not a heading"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_heading_no_space(self):
+        input = "#No space"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    #CODE TESTS
+    def test_code(self):
+        input = "```\nprint('hi')\n```"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.CODE)
+
+    def test_multiline_code(self):
+        input = "```\ncode block\nmore code\n```"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.CODE)
+
+    def test_code_wrong_ident(self):
+        input = "``\nprint('hi')\n``"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    #QUOTE TESTS
+    def test_quote(self):
+        input = ">quote"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.QUOTE)
+
+    def test_quote_space(self):
+        input = "> quote\n> another quote"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.QUOTE)
+
+    def test_heading_too_many_hash(self):
+        input = "> good\nnot a quote"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    #UNORDERED LIST TESTS
+    def test_ul(self):
+        input = "- one"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.UNORDERED_LIST)
+
+    def test_ul_multi(self):
+        input = "- one\n- two\n- three"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.UNORDERED_LIST)
+
+    def test_heading_too_many_hash(self):
+        input = "- one\nnot a list item"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    #ORDERED LIST TESTS
+    def test_ol(self):
+        input = "1. one"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.ORDERED_LIST)
+
+    def test_ol_multi(self):
+        input = "1. one\n2. two\n3. three"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.ORDERED_LIST)
+
+    def test_ol_numberskip(self):
+        input = "1. one\n3. three"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_ol_starts_wrong(self):
+        input = "2. starts wrong"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    #PARAGRAPH TESTS
+    def test_paragraph(self):
+        input = "just a normal paragraph"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_paragraph_multi(self):
+        input = "this is text\nthat does not match any special block"
+
+        block = markdown_to_blocks(input)[0]
+
+        result = block_to_block_type(block)
+
+        self.assertEqual(result, BlockType.PARAGRAPH)
 
 
 
