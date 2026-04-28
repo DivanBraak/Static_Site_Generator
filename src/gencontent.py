e37545dec68669939f0ed0178b14e2349af67d37
DivanB <divan.braak@gmail.com>
2026-04-28 20:57:17 +0200

    added recursive generation of all files in content to public

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,7 +1,9 @@
 from markdown_blocks import markdown_to_html_nodes,extract_title
 import os
+from pathlib import Path
 
 def generate_page(from_path, template_path, dest_path):
+
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     from_file = open(from_path)
@@ -23,3 +25,22 @@ def generate_page(from_path, template_path, dest_path):
     dest_file = open(dest_path, mode="w")
 
     dest_file.write(final)
+
+def generate_pages_recursive(dir_path_content,template_path,dest_dir_path):
+
+    content_directories = os.listdir(dir_path_content)
+    
+
+    for item in content_directories:
+
+        current_path = os.path.join(dir_path_content,item)
+
+        if os.path.isfile(current_path):
+
+            dest_path = os.path.join(dest_dir_path,item)
+            new_dest_path = (Path(dest_path).with_suffix(".html"))
+            generate_page(current_path,template_path,new_dest_path)
+        else:
+            new_dir = os.path.join(dir_path_content,item)
+            new_dest = os.path.join(dest_dir_path,item)
+            generate_pages_recursive(new_dir,template_path,new_dest)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from copystatic import copy_static_to_public
-from gencontent import generate_page
+from gencontent import generate_page,generate_pages_recursive
 
 def main():
     copy_static_to_public("static","public")
     
-    generate_page("content/index.md","template.html","public/index.html")
+    #generate_page("content/index.md","template.html","public/index.html")
+
+    generate_pages_recursive("content","template.html","public")
 
 
 
